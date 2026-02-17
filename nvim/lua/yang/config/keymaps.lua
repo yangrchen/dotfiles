f08d56e34e2e404e817326ffa3500c22e2baf905
Yang Chen <yang@usmbp2923-ychen.lan>
2026-02-17 15:11:07 -0500

    Added basic buffer keymaps

--- nvim/lua/yang/config/keymaps.lua
+++ nvim/lua/yang/config/keymaps.lua
@@ -1,10 +1,14 @@
 -- Better window navigation
-vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to left window' })
-vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to window below' })
-vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to window above' })
-vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to right window' })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to window below" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to window above" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 
 -- Window splits
-vim.keymap.set('n', '<leader>sv', '<C-w>v', { desc = 'Split vertically' })
-vim.keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'Split horizontally' })
-vim.keymap.set('n', '<leader>sq', '<C-w>q', { desc = 'Close window' })
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split vertically" })
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split horizontally" })
+vim.keymap.set("n", "<leader>sq", "<C-w>q", { desc = "Close window" })
+
+-- Closing buffers
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save" })
+vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit" })
