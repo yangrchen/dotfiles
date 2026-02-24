5663740e14023dd17c2a24138d782aa8b09b2130
Yang Chen <yang.chen@dataiku.com>
2026-02-24 13:33:19 -0500

    Changed autoindent options for Python

--- nvim/lua/yang/config/options.lua
+++ nvim/lua/yang/config/options.lua
@@ -4,4 +4,9 @@ vim.opt.number = true
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.g.python_indent = {
+    open_paren = "shiftwidth()",
+    closed_paren_align_last_line = false,
+}
+
 vim.opt.clipboard = "unnamedplus"
