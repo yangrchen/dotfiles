35961c70767c6d6bf15e99793294c66a25422308
Yang Chen <yang.chen@dataiku.com>
2026-02-25 11:18:25 -0500

    Modified Vim window keymaps. Changed Zed keymaps and settings

--- nvim/lua/yang/config/keymaps.lua
+++ nvim/lua/yang/config/keymaps.lua
@@ -1,13 +1,19 @@
 -- Better window navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to window below" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to window above" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+vim.keymap.set("n", "<leader>h", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<leader>j", "<C-w>j", { desc = "Move to window below" })
+vim.keymap.set("n", "<leader>k", "<C-w>k", { desc = "Move to window above" })
+vim.keymap.set("n", "<leader>l", "<C-w>l", { desc = "Move to right window" })
 
 -- Window splits
 vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split vertically" })
 vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split horizontally" })
 vim.keymap.set("n", "<leader>sq", "<C-w>q", { desc = "Close window" })
+
+-- Window move
+vim.keymap.set("n", "<leader>rh", "<C-w>H", { desc = "Move window left" })
+vim.keymap.set("n", "<leader>rj", "<C-w>J", { desc = "Move window down" })
+vim.keymap.set("n", "<leader>rk", "<C-w>K", { desc = "Move window up" })
+vim.keymap.set("n", "<leader>rl", "<C-w>L", { desc = "Move window right" })
 
 -- Closing buffers
 vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save" })
--- nvim/lua/yang/lazy/mini.lua
+++ nvim/lua/yang/lazy/mini.lua
@@ -4,7 +4,6 @@ return {
 		version = "*",
 		config = function()
 			require("mini.git").setup()
-			require("mini.comment").setup()
 			require("mini.pairs").setup()
 			require("mini.surround").setup()
 			require("mini.statusline").setup()
